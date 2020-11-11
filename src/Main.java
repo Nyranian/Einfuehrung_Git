b829576550f3183eca96c0750249f83e8ad6b5ab
ereining <ereining@htl-steyr.ac.at>
2020-11-11 12:10:16 +0100

    Änderungen: Conflict wird provoziert

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Juanson");
         System.out.println("I scheiß da indn Trockner");
         System.out.println("Aluakhbar");
+        System.out.println("ITP ist super!");
     }
 
 }
