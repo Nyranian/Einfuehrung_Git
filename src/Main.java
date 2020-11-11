d025def580e9e09b408170e52faf8418db2cc325
Elias <erodinge@htl-steyr.ac.at>
2020-11-11 11:47:25 +0100

    Erste Übertragung, Anlegen der Projektgrundstruktur

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Juanson");
         System.out.println("I scheiß da indn Trockner");
+        System.out.println("kot");
+
     }
 
 }
