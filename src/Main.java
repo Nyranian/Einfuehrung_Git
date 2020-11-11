c2e845fb91234579520a7c43948c7a75938ef767
Elias <erodinge@htl-steyr.ac.at>
2020-11-11 12:10:05 +0100

    Konflikt wird erzwungen

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Juanson");
         System.out.println("I scheiß da indn Trockner");
+
+        System.out.println("no");
     }
 
 }
